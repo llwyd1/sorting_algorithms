f3c2a8186477a9eb151d0451a2808b58b0bb9556
Allen Sibanda <allenalaska1@gmail.com>
2023-02-20 11:36:27 +0200

    finished task 2, added prototype to header file

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -21,4 +22,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size); /* task 0 */
 void swap(int *, int *);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 #endif
