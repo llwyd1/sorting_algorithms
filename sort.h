c0d0dbd00cb4c01ded6721b4897ea4e4cb155983
Allen Sibanda <allenalaska1@gmail.com>
2023-02-21 08:55:53 +0200

    refined existing code on task 2 and task 0

--- sort.h
+++ sort.h
@@ -24,4 +24,5 @@ void bubble_sort(int *array, size_t size); /* task 0 */
 void swap(int *, int *);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
